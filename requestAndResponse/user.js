3195f8095d856644d13cb24a859a042b82c199d2
gaurav4040 <jangragaurav593@gmail.com>
2025-02-10 17:43:45 +0530

    changes

--- requestAndResponse/user.js
+++ requestAndResponse/user.js
@@ -9,14 +9,18 @@
              res.setHeader('Content-Type','text/html');
              res.write('<html>');
              res.write('<head><title>Complete Coding</title><head>');
-             res.write('<body><h1>Welcome Home</h1></body>');
-             res.write('</html>');
-             return res.end();
-         }else if(req.url==='/products' ){
-             res.setHeader('Content-Type','text/html');
-             res.write('<html>');
-             res.write('<head><title>Complete Coding</title><head>');
-             res.write('<body><h1>Checkout new products</h1></body>');
+             res.write('<body><h1>Enter your details : </h1>');
+             res.write('<form>');
+             res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
+             res.write('<label for="male">Male</label>')
+             res.write('<input type="radio" name="gender" id="male" value="male" />')
+             res.write('<label for="female">Female</label>')
+             res.write('<input type="radio" name="gender" id="female" value="female" />')
+             res.write('<br><input type="submit" value="submit">');
+             res.write('</form');
+
+
+             res.write('</body>');
              res.write('</html>');
              return res.end();
          }
@@ -31,4 +35,4 @@
  const PORT = 3030;
  server.listen(PORT,()=>{
      console.log(`server is running on http:://localhost:${PORT}`)
- });
+ }); 
